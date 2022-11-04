65c3435bbd2b10bfc6792aea07ea90795f0d4dab
Parthiv7890 <88973956+Parthiv7890@users.noreply.github.com>
2022-11-04 16:56:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,20 @@ function setup(){
 }
 function draw(){
     image(video,0,0,380,380)
-    for(i = 0;i<objects.length;i++){
-        document.getElementById("status").innerHTML = "Status : Objects Detected"
-        fill(255,0,0)
-        confidencePercent = floor(objects[i].confidence * 100)
-        text(objects[i].label + " "+confidencePercent+"% ",objects[i].x+5,objects[i].y+15)
-        noFill()
-        stroke(255,0,0)
-        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+    
+    if(status!=""){
+        objDetector.detect(video,gotResults)
+        for(i = 0;i<objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
+            fill(255,0,0)
+            confidencePercent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " "+confidencePercent+"% ",objects[i].x+5,objects[i].y+15)
+            noFill()
+            stroke(255,0,0)
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }
     }
+    
 }
 function start(){
     objDetector = ml5.objectDetector('cocossd',modelLoaded)
